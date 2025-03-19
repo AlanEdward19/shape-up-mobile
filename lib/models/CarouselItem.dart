531d00a609d74f65f512fc8396e6d145fd41b402
AlanEdward19 <alan-edward@outlook.com.br>
2025-03-19 13:10:09 -0300

    feat: Implement Carousel and CarouselItem
    This commit introduces a new `CarouselItem` model and a carousel widget (`_Carousel`) to the main page.
    
    - Created `CarouselItem` model for carousel data.
    - Implemented `_Carousel` widget with automatic sliding and page indicators.
    - Added four `CarouselItem` instances for initial carousel content.
    - Replaced static text with dynamic `_Carousel` content.
    - Introduced `PageController` for manual control and automatic sliding.
    - Added page indicator dots to visually represent the current carousel item.
    - Incorporated a timer to automatically switch between carousel items every 10 seconds.

---
+++ lib/models/CarouselItem.dart
@@ -0,0 +1,6 @@
+class CarouselItem {
+  final String title;
+  final String description;
+
+  CarouselItem({required this.title, required this.description});
+}
--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,11 +1,35 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../models/CarouselItem.dart';
+
 class Main extends StatelessWidget {
-  const Main({super.key});
+  Main({super.key});
+
+  final List<CarouselItem> carouselItems = [
+    CarouselItem(
+        title: "ShapeUp",
+        description:
+        "Uma rede social inovadora que conecta profissionais de nutrição e educação física a clientes. Com ferramentas para prescrição de treinos e dietas, além de um ambiente interativo, o ShapeUp torna o acompanhamento acessível, eficiente e motivador."),
+    CarouselItem(
+        title: "Nutrição",
+        description:
+        "A alimentação é essencial para a saúde e o desempenho. No ShapeUp, nutricionistas criam planos personalizados, acompanham a evolução dos clientes e oferecem suporte contínuo para uma vida mais equilibrada."),
+    CarouselItem(
+        title: "Treino",
+        description:
+        "Cada pessoa tem um objetivo único, e o ShapeUp ajuda a alcançá-lo. Personal trainers montam treinos sob medida, acompanham o progresso e ajustam planos conforme a necessidade, garantindo eficiência e resultados."),
+    CarouselItem(
+        title: "Conexão",
+        description:
+        "A motivação cresce com o apoio certo. No ShapeUp, usuários compartilham conquistas, trocam experiências e se conectam com profissionais, transformando desafios em hábitos saudáveis e duradouros.")
+  ];
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 34),
@@ -21,25 +45,7 @@ class Main extends StatelessWidget {
 
               SizedBox(height: 45),
 
-              Text(
-                "ShapeUp",
-                style: const TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold
-                ),
-              ),
-
-              SizedBox(height: 15),
-
-              Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                style: const TextStyle(
-                    fontSize: 13,
-                  color: Colors.white,
-                    fontWeight: FontWeight.bold
-                ),
-              ),
+              _Carousel(carouselItems: carouselItems),
 
               SizedBox(height: 130),
 
@@ -90,5 +96,120 @@ class Main extends StatelessWidget {
               },
               child: const Text('Login'),
             );
+  }
+}
+
+class _Carousel extends StatefulWidget {
+  final List<CarouselItem> carouselItems;
+  const _Carousel({Key? key, required this.carouselItems}) : super(key: key);
+
+  @override
+  State<_Carousel> createState() => _CarouselState();
+}
+
+class _CarouselState extends State<_Carousel> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _startAutoSlide();
+    _pageController.addListener(() {
+      setState(() {
+        _currentPage = _pageController.page!.round();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void _startAutoSlide() {
+    _timer = Timer.periodic(const Duration(seconds: 10), (Timer timer) {
+      if (_currentPage < widget.carouselItems.length - 1) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+
+      if (_pageController.hasClients) {
+        _pageController.animateToPage(
+          _currentPage,
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeIn,
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 160,
+          child: PageView.builder(
+            controller: _pageController,
+            itemCount: widget.carouselItems.length,
+            itemBuilder: (context, index) {
+              return _buildCarouselItem(widget.carouselItems[index]);
+            },
+          ),
+        ),
+        const SizedBox(height: 10),
+        _buildPageIndicator(),
+      ],
+    );
+  }
+
+  Widget _buildCarouselItem(CarouselItem item) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Center(
+            child: Text(
+            item.title,
+            style: const TextStyle(
+              fontSize: 25,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+          ),
+        )),
+
+        const SizedBox(height: 15),
+
+        Text(
+          item.description,
+          style: const TextStyle(
+            fontSize: 13,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPageIndicator() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(widget.carouselItems.length, (index) {
+        return Container(
+          margin: const EdgeInsets.symmetric(horizontal: 15),
+          width: 10,
+          height: 10,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: _currentPage == index ? Color(0xFF169CD6) : Colors.white,
+          ),
+        );
+      }),
+    );
   }
 }
