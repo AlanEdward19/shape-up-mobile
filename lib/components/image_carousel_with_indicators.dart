6826bf2a7a533cef4f1810e4a348737bd833496e
AlanEdward19 <alan-edward@outlook.com.br>
2025-09-09 12:33:25 -0300

    feat: Add video support to image carousel with dynamic media handling

--- lib/components/image_carousel_with_indicators.dart
+++ lib/components/image_carousel_with_indicators.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class ImageCarouselWithIndicator extends StatefulWidget {
-  final List<String> imageUrls;
+  final List<String> mediaUrls;
 
-  const ImageCarouselWithIndicator({super.key, required this.imageUrls});
+  const ImageCarouselWithIndicator({super.key, required this.mediaUrls});
 
   @override
   State<ImageCarouselWithIndicator> createState() => _ImageCarouselWithIndicatorState();
@@ -21,7 +22,7 @@ class _ImageCarouselWithIndicatorState extends State<ImageCarouselWithIndicator>
           height: 300,
           child: PageView.builder(
             controller: _controller,
-            itemCount: widget.imageUrls.length,
+            itemCount: widget.mediaUrls.length,
             scrollDirection: Axis.horizontal,
             onPageChanged: (index) {
               setState(() {
@@ -29,18 +30,23 @@ class _ImageCarouselWithIndicatorState extends State<ImageCarouselWithIndicator>
               });
             },
             itemBuilder: (context, index) {
-              return Image.network(
-                widget.imageUrls[index],
-                fit: BoxFit.cover,
-                width: double.infinity,
-              );
+              final url = widget.mediaUrls[index];
+              if (isVideo(url)) {
+                return VideoPlayerWidget(videoUrl: url);
+              } else {
+                return Image.network(
+                  url,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                );
+              }
             },
           ),
         ),
         const SizedBox(height: 20),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: List.generate(widget.imageUrls.length, (index) {
+          children: List.generate(widget.mediaUrls.length, (index) {
             return AnimatedContainer(
               duration: const Duration(milliseconds: 300),
               margin: const EdgeInsets.symmetric(horizontal: 4),
@@ -55,5 +61,50 @@ class _ImageCarouselWithIndicatorState extends State<ImageCarouselWithIndicator>
         ),
       ],
     );
+  }
+
+  bool isVideo(String url) {
+    const videoExtensions = ['.mp4', '.webm', '.mov', '.ogg'];
+    final cleanUrl = url.split('?')[0].split('#')[0];
+    return videoExtensions.any((ext) => cleanUrl.toLowerCase().endsWith(ext));
+  }
+}
+
+class VideoPlayerWidget extends StatefulWidget {
+  final String videoUrl;
+
+  const VideoPlayerWidget({super.key, required this.videoUrl});
+
+  @override
+  State<VideoPlayerWidget> createState() => _VideoPlayerWidgetState();
+}
+
+class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl))
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller.value.isInitialized
+        ? AspectRatio(
+      aspectRatio: _controller.value.aspectRatio,
+      child: VideoPlayer(_controller),
+    )
+        : const Center(child: CircularProgressIndicator());
   }
 }
