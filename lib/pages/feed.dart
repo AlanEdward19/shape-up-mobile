c72f3764a7760a67818a0741bd48f76a56f03099
AlanEdward19 <alan-edward@outlook.com.br>
2025-09-10 12:35:01 -0300

    feat: Update reaction popup dimensions and emoji size for better responsiveness

--- lib/pages/feed.dart
+++ lib/pages/feed.dart
@@ -233,12 +233,14 @@ class _FeedState extends State<Feed> with RouteAware {
     final Offset buttonPosition = buttonBox.localToGlobal(Offset.zero);
     final Size buttonSize = buttonBox.size;
     final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    final emojiSize = screenWidth * 0.043;
 
-    const double popupHeight = 70.0;
-    const double popupWidth = 280.0;
+    double popupHeight = screenHeight * 0.1;
+    double popupWidth = screenWidth * 0.2;
 
     double top = buttonPosition.dy - popupHeight - 8;
-    double left = buttonPosition.dx + buttonSize.width / 2 - popupWidth / 2;
+    double left = (buttonPosition.dx + buttonSize.width / 2 - popupWidth / 2) -10;
 
     if (top < kToolbarHeight) {
       top = buttonPosition.dy + buttonSize.height - 100;
@@ -258,6 +260,7 @@ class _FeedState extends State<Feed> with RouteAware {
                 child: Material(
                   type: MaterialType.transparency,
                   child: ReactionPopup(
+                    emojiSize: emojiSize,
                     onEmojiSelected: (reactionType) {
                       Navigator.of(dialogContext).pop();
                       _handleReactionSelected(postId, reactionType);
